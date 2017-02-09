6d45e3356d5470b01ad8c0e30f655083667f839e
palakparekh <palakvparekh@gmail.com>
2017-02-09 23:03:10 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,6 @@
+length=int(input("enter length of rectangle:"))
+breadth=int(input("enter breadth of rectangle:"))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("\n area of rectangle is:%2d" %area)
+print("\n perimeter of rectangle is:%2d" %perimeter)
