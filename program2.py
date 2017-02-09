1225abc7c8704f7596031dba06156b822d8e2bbf
palakparekh <palakvparekh@gmail.com>
2017-02-09 23:13:10 +0530

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,6 @@
+PI=3.14
+radius=int(input("enter radius of circle:"))
+area=PI*radius*radius
+circumference=2*PI*radius
+print("area of circle=%2d"%area)
+print("circumference of circle=%2d"%circumference)
